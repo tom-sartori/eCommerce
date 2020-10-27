139327b21a851cde2906b52dfa80879b50a3e328
Tom SARTORI <tom.sartori-letourneau@etu.umontpellier.fr>
2020-10-27 12:17:26 +0100

    up

--- lib/File.php
+++ lib/File.php
@@ -3,7 +3,7 @@
 class File {
 	public static function build_path($path_array) {
 		$DS = DIRECTORY_SEPARATOR; 
-		$ROOT_FOLDER = "/home/ann2/sartorit/public_html/eCommerce" . $DS . ".."; 
+		$ROOT_FOLDER = __DIR__ . $DS . ".."; 
 		return $ROOT_FOLDER. $DS . join($DS, $path_array);
 	}
 }
