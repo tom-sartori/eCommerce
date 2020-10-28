1745c5c24d91807d100621599880f011250591fc
tom-sartori <60385058+tom-sartori@users.noreply.github.com>
2020-10-28 19:19:39 +0100

    Update detail.php

--- view/Fournisseur/detail.php
+++ view/Fournisseur/detail.php
@@ -5,7 +5,7 @@ $f_htmlIdFournisseur=htmlspecialchars($f->getIdFournisseur());
 $f_htmlAdresse=htmlspecialchars($f->getAdresse());
 $f_htmlAdresseMail=htmlspecialchars($f->getAdresseMail());
 $f_htmlPays=htmlspecialchars($f->getPays());
-echo '<p> '.'Le fournisseur du nom de : ' . $f_htmlNom . ', son adresse mail est : '. $f_htmlAdresseMail . ' qui se situe au ' . $f_htmlAdresse  . 'qui se trouve en ' . $f_htmlPays
+echo '<p> '.'Le fournisseur du nom de : ' . $f_htmlNom . ', son adresse mail est : '. $f_htmlAdresseMail . ' qui se situe au ' . $f_htmlAdresse  . ' qui se trouve en ' . $f_htmlPays
     .' d\'identifiant '. $f_htmlIdFournisseur.' '.
     '<a href="./index.php?controller=Fournisseur&action=delete&idFournisseur=' . $f_raw  . '">'. '<button>' .'Supprimer ce fournisseur'.'</button>' .'</a>'. ' '
     .'<a href="./index.php?controller=Fournisseur&action=update&idFournisseur=' . $f_raw  . '">'. '<button>'.'Mettre à jour le fournisseur'.'</button>' .'</a>'.'</p>'
