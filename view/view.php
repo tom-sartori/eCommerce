1deab45b71c5260bd5c6d84e9b939cf4e8d5f789
tom-sartori <60385058+tom-sartori@users.noreply.github.com>
2020-11-12 12:54:01 +0100

    Update view.php

--- view/view.php
+++ view/view.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    link rel="stylesheet" type="text/css" href="./css/general.css"
+    <link rel="stylesheet" href="./css/general.css">
     <meta charset="UTF-8">
     <title><?php echo $pagetitle; ?></title>
 </head>
