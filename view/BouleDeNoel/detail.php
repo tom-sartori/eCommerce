a49cc1759eec3e64b904e53ca5e7935b90752fa0
Tom SARTORI <tom.sartori-letourneau@etu.umontpellier.fr>
2020-11-19 16:29:01 +0100

    Clarification code.

--- view/BouleDeNoel/detail.php
+++ view/BouleDeNoel/detail.php
@@ -9,16 +9,6 @@ $b_htmlMatiere=htmlspecialchars($b->getMatiere());
 $b_htmlStocks=htmlspecialchars($b->getStock());
 $b_htmlFournisseur=htmlspecialchars($b->getIdFournisseur());
 
-echo '<p> '.'Numéro de série: ' . $b_htmlIdBouleDeNoel . '<br>'.
-    'Nom : '. $b_htmlNom . '<br>'.
-    'Couleur : ' . $b_htmlCouleur . '<br>'.
-    'Taille : ' . $b_htmlTaille .' cm' .'<br>'.
-    'Matiere : ' . $b_htmlMatiere . '<br>'.
-    'Stocks : '. $b_htmlStocks . '<br>'.
-    'Fournit  par : '. '<a href="./index.php?controller=Fournisseur&action=read&idFournisseur=' . $b_rawFournisseur . '">' . $b_htmlFournisseur . '</a>' . '<br>'.
-    '<a href="./index.php?controller=BouleDeNoel&action=delete&idBouleDeNoel=' . $b_raw  . '">'. '<button>' .'Supprimer cette boule de noël'.'</button>' .'</a>'. ' '
-    .'<a href="./index.php?controller=BouleDeNoel&action=update&idBouleDeNoel=' . $b_raw  . '">'. '<button>'.'Mettre à jour la boule de noël'.'</button>' .'</a>'.'</p>';
-
 echo <<< EOT
     <p> 
         Numéro de série : {$b_htmlIdBouleDeNoel} 
