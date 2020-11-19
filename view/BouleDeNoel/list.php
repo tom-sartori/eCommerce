5781a4764c7aa33e85be9c030125d2d4cd2c7149
Tom SARTORI <tom.sartori-letourneau@etu.umontpellier.fr>
2020-11-19 16:57:13 +0100

    Clarification code.

--- view/BouleDeNoel/list.php
+++ view/BouleDeNoel/list.php
@@ -1,8 +1,9 @@
 <?php
 
 foreach ($tab_b as $b) {
+    var_dump($b->get('idBouleDeNoel'));
     $b_raw = rawurlencode($b->get('idBouleDeNoel'));
-    $b_spe = htmlspecialchars($b->get('IdBouleDeNoel'));
+    $b_spe = htmlspecialchars($b->get('idBouleDeNoel'));
     echo '<p> ' . 'Boule de noel de numero de série : ' .
         '<a href="./index.php?controller=BouleDeNoel&action=read&idBouleDeNoel=' . $b_raw . '">' . $b_spe . '</a>' . " " .
         '<a href="./index.php?controller=BouleDeNoel&action=delete&idBouleDeNoel=' . $b_raw . '">' . '<button>' . 'Supprimer cette boule de noel' . '</button>' .
