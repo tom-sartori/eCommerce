e6490e562a67b1cdaf15052ddad0a7bc23180335
Tom SARTORI <tom.sartori-letourneau@etu.umontpellier.fr>
2020-11-27 14:01:45 +0100

    Clear code.

--- view/BouleDeNoel/updated.php
+++ view/BouleDeNoel/updated.php
@@ -1,6 +1,4 @@
-<div>
-	<?php
-        echo '<p> La boule de Noël a bien été mise à jour dans la base de données. </p> ';
-        require (File::build_path(Array("view","BouleDeNoel","list.php")));
-	?>
-</div>
+<?php
+    echo '<p> La boule de Noël a bien été mise à jour dans la base de données. </p> ';
+    require (File::build_path(Array("view","BouleDeNoel","list.php")));
+?>
--- view/Utilisateur/list.php
+++ view/Utilisateur/list.php
@@ -2,12 +2,12 @@
 
 foreach ($tab_u as $u) {
     $u_raw = rawurlencode($u->get('login'));
-    $u_spe = htmlspecialchars($u->get('login'));
+    $u_html = htmlspecialchars($u->get('login'));
     echo <<< EOT
         <p>
             Utilisateur de login : 
             <a href="./index.php?controller=Utilisateur&action=read&login={$u_raw}">
-                {$u_spe}
+                {$u_html}
             </a>
             <br>
             <a href="./index.php?controller=Utilisateur&action=delete&login={$u_raw}">
@@ -18,3 +18,4 @@ EOT;
 }
 
 echo '<a href="./index.php?controller=Utilisateur&action=create"><button>Ajouter un utilisateur</button></a>';
+?>
