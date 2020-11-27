2b2a2e4852432ea017d8df4529b147c494cf06f7
Tom SARTORI <tom.sartori-letourneau@etu.umontpellier.fr>
2020-11-27 14:48:44 +0100

    css

--- view/view.php
+++ view/view.php
@@ -10,11 +10,9 @@
 
     <body>
         <nav>
-            <p style=" border: 1px solid black;text-align: center ;padding-right:1em;">
             <a href="index.php?controller=BouleDeNoel&action=readAll"> <strong>Liste des boules de noel</strong></a>
             <a href="index.php?controller=Utilisateur&action=readAll"> <strong>Liste des utilisateurs</strong></a>
             <a href="index.php?controller=Fournisseur&action=readAll"> <strong>Liste des Fournisseurs</strong></a>
-            </p>
         </nav>
 
         <main>
@@ -25,9 +23,9 @@
         </main>
 
         <footer>
-            <p>
-                Site de ventes de boules de noël
-            </p>
+            <h3>
+                Des boules pour tous
+            </h3>
         </footer>
     </body>
 </html>
