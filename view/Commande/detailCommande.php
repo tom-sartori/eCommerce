67f375db7dd2737140ea7f54ee9069037d72ffaa
Tom SARTORI <tom.sartori-letourneau@etu.umontpellier.fr>
2020-12-16 21:26:46 +0100

    Améliorations merge.

--- view/Commande/detailCommande.php
+++ view/Commande/detailCommande.php
@@ -1,18 +1,39 @@
 <?php
-echo'Voici vos commandes : <br> ';
-$memoire=0;
-foreach ($tab_c as $c){
-    $c_htmllogin = htmlspecialchars($c->get('login'));
-    $c_htmlIdBouleDeNoel = htmlspecialchars($c->get('idBouleDeNoel'));
-    $c_htmlidCommande = htmlspecialchars($c->get('idCommande'));
-    $c_htmldateAchat = htmlspecialchars($c->get('dateAchat'));
-    $c_htmlquantite = htmlspecialchars($c->get('quantite'));
-    if($memoire==$c_htmlidCommande){
-        echo $c_htmlquantite .' boules de noel d\'identifiant ' . $c_htmlIdBouleDeNoel . '<br>';
-    } else {
-        $memoire = $c->get('idCommande');
-        echo '<br>La Commande N° ' . $c_htmlidCommande . ' faite le ' . $c_htmldateAchat . ', contient les articles suivant :
-        <br>'
-            . $c_htmlquantite . ' boules de noel d\'identifiant ' . $c_htmlIdBouleDeNoel . '<br>';
+    echo'<h1>Voici vos commandes : </h1>';
+    $memoire=0;
+    foreach ($tab_c as $c){
+        $c_htmllogin = htmlspecialchars($c->get('login'));
+        $c_htmlIdBouleDeNoel = htmlspecialchars($c->get('idBouleDeNoel'));
+        $c_htmlidCommande = htmlspecialchars($c->get('idCommande'));
+        $c_htmldateAchat = htmlspecialchars($c->get('dateAchat'));
+        $c_htmlquantite = htmlspecialchars($c->get('quantite'));
+
+        if($memoire == $c_htmlidCommande)
+            echo <<< EOT
+                    <li>
+                        <p>
+                            {$c_htmlquantite} boules de noel d'identifiant {$c_htmlIdBouleDeNoel} 
+                        </p>
+                    </li>
+EOT;
+        else {
+            if ($memoire != 0)
+                echo '</ul>';
+
+            $memoire = $c->get('idCommande');
+            echo <<< EOT
+                    <br> 
+                    <h4> 
+                        La Commande N° {$c_htmlidCommande} faite le {$c_htmldateAchat}, contient les articles suivants : 
+                    </h4>
+                    <ul>
+                        <li>
+                            <p>
+                                {$c_htmlquantite} boules de noel d'identifiant {$c_htmlIdBouleDeNoel}
+                            </p>
+                        </li>
+EOT;
+        }
     }
-}
+    echo '</ul>';
+?>
